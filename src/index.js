57a45d35d0ca5c931b5bb8bd135f1b1af251e3b4
Aliya <aliyuwe4ka@gmail.com>
2021-03-14 22:09:10 +0300

    feat: solving the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length <= 1) {
+    return false;
+  }
+
+  console.log(str)
+  let stack = [];
+  let pairs = bracketsConfig.reduce(function (acc, current) {
+    return { ...acc, [current[0]]: current[1] }
+  }, {})
+
+  let brachetsC;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
+      stack.push(str[i]);
+    } else {
+      let last = stack.pop();
+      if (str[i] !== pairs[last]) {
+        return false
+      }
+    }
+  }
+  if (stack.length !== 0) { return false };
+  return true
+
 }
